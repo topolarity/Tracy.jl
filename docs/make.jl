b4dbcbfab1093c1db266e38b5dc29091710a5778
Cody Tapscott <topolarity@tapscott.me>
2023-04-27 13:53:58 -0400

    docs: Add `using Tracy` to doctests

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter, Tracy
 
+DocMeta.setdocmeta!(Tracy, :DocTestSetup, :(using Tracy); recursive=true)
 makedocs(modules = [Tracy], sitename="Tracy.jl")
 
 deploydocs(repo = "github.com/topolarity/Tracy.jl.git")
