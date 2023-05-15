4947b28cbf8395d9d0f14774d9d861f61c6862a5
KristofferC <kristoffer.carlsson@juliacomputing.com>
2023-05-15 15:25:00 +0200

    add a test for a precompiled package
    This checks that we are correctly updating the pointers when a precompiled package is loaded

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 module BasicTests
     using Tracy
     using Test
+    using Pkg
 
     @tracepoint "test tracepoint" begin
 	    println("Hello, world!")
@@ -9,4 +10,12 @@ module BasicTests
     @test_throws ErrorException @tracepoint "test exception" begin
         error("oh no!")
     end
+
+    Pkg.activate("TestPkg")
+    Pkg.develop(; path = joinpath(@__DIR__, ".."))
+    # Test that a precompiled package also works,
+    # Can also be manually verified by attaching Tracy to this process
+    # withenv("JULIA_WAIT_FOR_TRACY"=>"1", "TRACY_PORT"=>"9000") do
+        run(`$(Base.julia_cmd()) --project="TestPkg" -e 'using TestPkg; TestPkg.time_something(); TestPkg.test_data()'`)
+    # end
 end
